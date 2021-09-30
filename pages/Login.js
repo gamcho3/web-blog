984eb14cf4b7c58457280fc4e855ec15e1d7e04a
Yong Jin <70016523+gamcho3@users.noreply.github.com>
2021-09-30 11:42:26 +0900

    edit loginpage

--- pages/Login.js
+++ pages/Login.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from "react";
 import Login from "../component/auth/Login";
 import Signup from "../component/auth/Signup";
 import { getSession } from "next-auth/client";
-
+import Head from "next/head";
 const LoginPage = (props) => {
   const [convert, setConvert] = useState(false);
 
@@ -12,6 +12,10 @@ const LoginPage = (props) => {
 
   return (
     <Fragment>
+      <Head>
+        <title>auth page</title>
+        <meta name="description" content="login and signup"></meta>
+      </Head>
       {!convert ? (
         <Login onClick={switchHandler} />
       ) : (
@@ -24,14 +28,14 @@ const LoginPage = (props) => {
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
 
-  // if (session) {
-  //   return {
-  //     redirect: {
-  //       destination: "/",
-  //       permanent: false,
-  //     },
-  //   };
-  // }
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: { session },
